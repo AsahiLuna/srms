10e1d82ae67f334637b13ce86e0a8d70cf396ba9
Asahi Zhang <asahi@alpgo.cc>
2019-08-04 20:38:07 +0800

    update

--- product/src/main/java/cc/alpgo/srms/product/ProductApplication.java
+++ product/src/main/java/cc/alpgo/srms/product/ProductApplication.java
@@ -2,16 +2,34 @@ package cc.alpgo.srms.product;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.reactive.FluxSender;
+import org.springframework.cloud.stream.reactive.StreamEmitter;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableReactiveMongoRepositories
+//@EnableBinding({Source.class, Sink.class, Processor.class})
+@EnableBinding({Source.class, Sink.class})
 public class ProductApplication extends AbstractReactiveMongoConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
     }
@@ -25,4 +43,20 @@ public class ProductApplication extends AbstractReactiveMongoConfiguration {
     protected String getDatabaseName() {
         return "test";
     }
+
+    @StreamEmitter
+    public void emit(@Output(Source.OUTPUT) FluxSender output) {
+        output.send(Flux.interval(Duration.ofSeconds(1)).map(l -> "Hello World"));
+    }
+
+    @StreamListener
+    public void receive(@Input(Sink.INPUT) Flux<String> inputs) {
+        inputs.map(String::toUpperCase)
+                .subscribe(input -> LOGGER.info("Received: {}", input));
+    }
+
+//    @StreamListener
+//    public void receive(@Input(Processor.INPUT) Flux<String> input, @Output(Processor.OUTPUT) FluxSender output) {
+//        output.send(input.map(s -> s.toUpperCase()));
+//    }
 }
